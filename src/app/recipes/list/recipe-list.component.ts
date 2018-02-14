bb0c1d7e3915342ba5c5ba704026862fea8cc123
Dmitriy Butakov <dmitry404@gmail.com>
2018-02-14 17:03:15 +0200

    added Recipe model

--- src/app/recipes/list/recipe-list.component.ts
+++ src/app/recipes/list/recipe-list.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [];
 }
