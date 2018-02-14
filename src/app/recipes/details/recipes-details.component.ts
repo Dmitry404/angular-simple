475c37e8757cb6b82c4daa8aa5683c792e442a14
Dmitriy Butakov <dmitry404@gmail.com>
2018-02-14 16:32:43 +0200

    project app components (planned)

---
+++ src/app/recipes/details/recipes-details.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-details',
+  template: '<p>recipe-details</p>',
+})
+export class RecipeDetailsComponent {
+}
---
+++ src/app/recipes/list/item/recipe-list-item.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list-item',
+  template: 'recipe list item',
+})
+export class RecipeListItemComponent {
+}
