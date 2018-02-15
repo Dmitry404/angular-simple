80e9e8c99e8d98626396ff36564787f789234823
Dmitriy Butakov <dmitry404@gmail.com>
2018-02-15 16:51:57 +0200

    local reference in template

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,27 +1,28 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
 })
 export class CockpitComponent {
-  serverName: string;
+  // serverName: string;
   serverContent: string;
+  @ViewChild('serverNameInput') serverNameInputElementRef: ElementRef;
 
   // @Output('srvCreated') this is an alias
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output() blueprintCreated = new EventEmitter<{blueprintName: string, blueprintContent: string}>();
 
-  onAddServer() {
+  onAddServer(serverNameInput) {
     this.serverCreated.emit({
-      serverName: this.serverName,
+      serverName: serverNameInput.value,
       serverContent: this.serverContent
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(serverNameInput) {
     this.blueprintCreated.emit({
-      blueprintName: this.serverName,
+      blueprintName: this.serverNameInputElementRef.nativeElement.value,
       blueprintContent: this.serverContent
     });
   }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.None, // Native (clears out the styles? shadow DOM), Emulated (default)
 })
 export class ServerElementComponent {
   // @Input('srvElement') - it's an alias. In such a case you should use it in a component (html) instead of 'element'
