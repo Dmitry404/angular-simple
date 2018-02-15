f12d80604f1773071b2fd0306de6dad4e0786760
Dmitriy Butakov <dmitry404@gmail.com>
2018-02-15 17:06:26 +0200

    Learning few lifecycle hooks

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,11 +6,24 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.None, // Native (clears out the styles? shadow DOM), Emulated (default)
 })
-export class ServerElementComponent {
+export class ServerElementComponent implements OnInit, OnChanges {
   // @Input('srvElement') - it's an alias. In such a case you should use it in a component (html) instead of 'element'
   @Input() element: {
     type: string,
     name: string,
     content: string,
   };
+
+  constructor() {
+    console.log('Constructor called');
+  }
+
+  ngOnInit(): void {
+    console.log('Called once a component initialized');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Called after a bound property changes');
+    console.log(changes);
+  }
 }
