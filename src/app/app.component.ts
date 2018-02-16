0ff3e03a0dd49a515fa97fe2112d2265abd3af89
Dmitriy Butakov <dmitry404@gmail.com>
2018-02-16 14:11:18 +0200

    my naive implementation of menu switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showRecipes = false;
+  showShoppingList = false;
+
+  onMenuClickEvent(menuName) {
+    if (menuName === 'recipes') {
+      this.showRecipes = true;
+      this.showShoppingList = false;
+    } else if (menuName === 'shoppingList') {
+      this.showRecipes = false;
+      this.showShoppingList = true;
+    } else {
+      this.showRecipes = false;
+      this.showShoppingList = false;
+    }
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  @Output() menuClickEvent = new EventEmitter<string>();
+
+  onRecipesClick() {
+    this.menuClickEvent.emit('recipes');
+  }
+
+  onShoppingListClick() {
+    this.menuClickEvent.emit('shoppingList');
+  }
 }
