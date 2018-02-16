1d582aad39d46bae812ec2a3adf6fc009f95a865
Dmitriy Butakov <dmitry404@gmail.com>
2018-02-16 15:30:28 +0200

    simple implementation of event propagation through parent to an adjasent component

--- src/app/recipes/list/item/recipe-list-item.component.ts
+++ src/app/recipes/list/item/recipe-list-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Recipe} from "../../recipe.model";
 
 @Component({
@@ -7,4 +7,9 @@ import {Recipe} from "../../recipe.model";
 })
 export class RecipeListItemComponent {
   @Input('recipeInput') recipe: Recipe;
+  @Output() recipeSelectedEvent = new EventEmitter<void>();
+
+  recipeSelected() {
+    this.recipeSelectedEvent.emit();
+  }
 }
--- src/app/recipes/list/recipe-list.component.ts
+++ src/app/recipes/list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -10,4 +10,9 @@ export class RecipeListComponent {
     new Recipe('Dummy Recipe - Cake #1', 'dummy recipe description', 'https://images.media-allrecipes.com/userphotos/250x250/636110.jpg'),
     new Recipe('Dummy Recipe - Cake #2', 'dummy recipe description 2', 'http://images.media-allrecipes.com/userphotos/250x250/708879.jpg')
   ];
+  @Output() recipeSelectedEvent = new EventEmitter<Recipe>();
+
+  recipeSelected(recipe: Recipe) {
+    this.recipeSelectedEvent.emit(recipe);
+  }
 }
