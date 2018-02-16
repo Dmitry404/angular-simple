86554ed5f411d7d55d38ce5b039933fc2eaed842
Dmitriy Butakov <dmitry404@gmail.com>
2018-02-16 17:54:45 +0200

    Using property binding in my custom directive

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,25 +1,32 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'yellowgreen';
+  @Input() defaultColor: string = 'yellowgreen';
+  @Input() hightlightColor: string = 'yellow';
+  @Input() afterHightlightColor: string = 'lightyellow';
+
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {
   }
 
   ngOnInit(): void {
     //this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'yellowgreen');
+    this.backgroundColor = this.defaultColor;
   }
 
   @HostListener('mouseenter') mouseOver(eventData: Event) {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'yellow');
-    this.backgroundColor = 'yellow';
+    // this.backgroundColor = 'yellow';
+    this.backgroundColor = this.hightlightColor;
   }
 
   @HostListener('mouseleave') mouseLeave(eventData: Event) {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'lightyellow');
-    this.backgroundColor = 'lightyellow';
+    // this.backgroundColor = 'lightyellow';
+    this.backgroundColor = this.afterHightlightColor;
   }
 }
