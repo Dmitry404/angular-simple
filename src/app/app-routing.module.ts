7bfff3bbf1a5b95364d6117aa60c7d35e7d36886
Dmitriy Butakov <dmitry404@gmail.com>
2018-02-20 15:41:37 +0200

    request confirmation when going away with unsaved server data (using CanDeactivate guard)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { ServerComponent } from "./servers/server/server.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { UserComponent } from "./users/user/user.component";
 import { AuthGuard } from "./auth-guard.service";
+import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -17,7 +18,7 @@ const appRoutes: Routes = [
   },
   { path: 'servers', /*canActivate: [AuthGuard],*/ canActivateChild: [AuthGuard], component: ServersComponent, children: [
       { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent },
+      { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] },
     ]
   },
   { path: 'page-not-found', component: PageNotFoundComponent },
