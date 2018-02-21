b2c1c38aa47083e530ad01bc4d123e47d47fb182
Dmitriy Butakov <dmitry404@gmail.com>
2018-02-21 16:19:20 +0200

    playing with forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('form') signupForm: NgForm;
+  answer: string;
+  genders = ['male', 'femail'];
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit(form: ElementRef) {
+    console.log(form);
+    console.log(this.signupForm);
   }
 }
