edc89666103f24aef5e6b67fdfac5cef902584b3
Dmitriy Butakov <dmitry404@gmail.com>
2018-02-21 17:52:49 +0200

    Forms template-driven assignment  (#5)
    * start point
    
    * form implemented
    
    * added validators and data 'processing'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('form') subscriptionForm: NgForm;
+  subscriptionData = {
+    email: '',
+    password: '',
+    subscription: '',
+  };
+  subscriptionSubmitted = false;
+
+  onFormSubmit() {
+    this.subscriptionData.email = this.subscriptionForm.value.email;
+    this.subscriptionData.password = this.subscriptionForm.value.password;
+    this.subscriptionData.subscription = this.subscriptionForm.value.subscription;
+
+    this.subscriptionSubmitted = true;
+    this.subscriptionForm.reset();
+  }
 }
