b878da21f28685230442cdbbc5226fc22ea74988
Dmitriy Butakov <dmitry404@gmail.com>
2018-02-22 12:24:37 +0200

    Forms reactive driven assignment  (#6)
    * basic form implementation
    
    * fixed errors and added validation with timeout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
+import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+
+  forbiddenProjectNames = ['Test'];
+
+  ngOnInit(): void {
+    this.projectForm = new FormGroup({
+      'name': new FormControl(null, Validators.required, this.containsForbiddenName.bind(this)),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'status': new FormArray([
+        new FormControl('Stable'),
+        new FormControl('Critical'),
+        new FormControl('Finished'),
+      ]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projectForm);
+  }
+
+  containsForbiddenName(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (this.forbiddenProjectNames.indexOf(control.value) !== -1) {
+          resolve({'nameIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 2500);
+    });
+    return promise;
+  }
 }
