92e046133939b9890aba85ef6285816978e64cb1
Dmitriy Butakov <dmitry404@gmail.com>
2018-02-23 14:32:33 +0200

    show image preview

--- src/app/recipes/details/recipes-details.component.ts
+++ src/app/recipes/details/recipes-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from "../recipe.model";
 import { RecipeService } from "../recipe.service";
-import { ActivatedRoute, Params } from "@angular/router";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 
 @Component({
   selector: 'app-recipe-details',
@@ -12,6 +12,7 @@ export class RecipeDetailsComponent implements OnInit {
   id: number;
 
   constructor(private recipeService: RecipeService,
+              private router: Router,
               private route: ActivatedRoute) {
   }
 
@@ -28,5 +29,6 @@ export class RecipeDetailsComponent implements OnInit {
 
   onDeleteRecipe() {
     this.recipeService.deleteRecipe(this.id);
+    this.router.navigate(['../'], {relativeTo: this.route});
   }
 }
