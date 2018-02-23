d16d926cdab72a72daadfb7716fbadf1c76a511f
Dmitriy Butakov <dmitry404@gmail.com>
2018-02-23 14:42:33 +0200

    unsubscribing from recipe changes on destroy

--- src/app/recipes/list/recipe-list.component.ts
+++ src/app/recipes/list/recipe-list.component.ts
@@ -1,22 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from "../recipe.service";
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
+
+  private recipeChangedSubscription: Subscription;
 
   constructor(private recipeService: RecipeService) {
   }
 
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
-    this.recipeService.recipeChanged.subscribe(
+    this.recipeChangedSubscription = this.recipeService.recipeChanged.subscribe(
       (recipes: Recipe[]) => {
         this.recipes = recipes;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.recipeChangedSubscription.unsubscribe();
   }
 }
